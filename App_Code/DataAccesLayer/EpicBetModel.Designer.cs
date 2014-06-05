251065bf1af7a1332e0e35e25192cfcdbb0ccdd9
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2014-06-05 21:32:19 +0200

    add price

--- App_Code/DataAccesLayer/EpicBetModel.Designer.cs
+++ App_Code/DataAccesLayer/EpicBetModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Xav\Documents\Visual Studio 2013\Projects\EpicWorldCupBet\EpicWorldCupBet\DataAccesLayer\EpicBetModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Xav\Desktop\EpicWorldCupBet\EpicWorldCupBet\App_Code\DataAccesLayer\EpicBetModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
