b086c1d6076dd16528618ed3c23de4958671dde3
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2015-01-14 19:43:14 +0100

    again

--- App_Code/SignalR/SQL/UserInformations.cs
+++ App_Code/SignalR/SQL/UserInformations.cs
@@ -30,7 +30,7 @@ namespace SignalR.SQL
             }
         }
 
-        public static List<Datas.Entities.Universe> GetUserUniverses(string user)
+        public static List<Universe> GetUserUniverses(string user)
         {
             using (var context = new Entities())
             {
